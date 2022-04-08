29e3af5edc24cb0ffb7dfb3d2acdb574c9224fe6
Erregoui Ismail <81243639+ErregouiIsmail@users.noreply.github.com>
2022-04-08 15:09:23 +0000

    camera preview using camerax

---
+++ MathResolverApp/app/src/main/java/me/otmane/mathresolver/Constants.kt
@@ -0,0 +1,13 @@
+package me.otmane.mathresolver
+
+
+import android.Manifest
+
+object Constants {
+    const val TAG  = "cameraX"
+    const val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss-SS"
+    const val REQUEST_CODE_PERMISSIONS = 123
+    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+
+}
--- MathResolverApp/app/src/main/java/me/otmane/mathresolver/MainActivity.kt
+++ MathResolverApp/app/src/main/java/me/otmane/mathresolver/MainActivity.kt
@@ -1,24 +1,127 @@
 package me.otmane.mathresolver
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import me.otmane.mathresolver.databinding.MainActivityBinding
 import me.otmane.mathresolver.ui.main.MainFragment
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: MainActivityBinding
+    private var imageCapture: ImageCapture? = null
+    private  lateinit var outputDirectory: File
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = MainActivityBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+
+        outputDirectory = getOutputDirectory()
+
+        if (allPermissionGranted()){
+            startCamera()
+        }
+        else{
+            ActivityCompat.requestPermissions(this, Constants.REQUIRED_PERMISSIONS,Constants.REQUEST_CODE_PERMISSIONS)
+        }
+        binding.captureButton.setOnClickListener{
+            takePhoto()
         }
     }
+    private fun getOutputDirectory():File{
+        val mediaDir = externalMediaDirs.firstOrNull()?.let {mFile ->
+            File(mFile, resources.getString(R.string.app_name)).apply {
+                mkdirs()
+            }
+        }
+        return if(mediaDir != null && mediaDir.exists())
+            mediaDir else filesDir
+    }
+
+
+    private fun takePhoto(){
+        val imageCapture = imageCapture?:return
+        val photoFile = File(
+            outputDirectory,
+            SimpleDateFormat(Constants.FILE_NAME_FORMAT,
+                Locale.getDefault()).format(System.currentTimeMillis()) +".jpg")
+       val outputOption = ImageCapture.OutputFileOptions.Builder(photoFile).build()
+        imageCapture.takePicture(outputOption,ContextCompat.getMainExecutor(this), object :ImageCapture.OnImageSavedCallback{
+            override fun onImageSaved(outputFileResults: ImageCapture.OutputFileResults) {
+                val savedUri = Uri.fromFile(photoFile)
+                val msg = "Photo saved"
+                Toast.makeText(this@MainActivity, "$msg $savedUri",Toast.LENGTH_LONG).show()
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                Log.e(Constants.TAG, "onError: ${exception.message}",exception)
+            }
+
+        })
+    }
+
+
+    private fun startCamera(){
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val  preview =  Preview.Builder()
+                .build()
+                .also { mPreview ->
+                    mPreview.setSurfaceProvider(
+                        binding.viewFinder.surfaceProvider
+                    )
+                }
+            imageCapture = ImageCapture.Builder()
+                .build()
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture, )
+            }catch (e:Exception){
+                Log.d(Constants.TAG, "startCamera Fail:", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == Constants.REQUEST_CODE_PERMISSIONS){
+          if (allPermissionGranted()){
+              startCamera()
+          }
+          else{
+              Toast.makeText(this,"Permissions not granted by the user", Toast.LENGTH_SHORT).show()
+              finish()
+          }
+      }
+
+    }
+
+
+    private fun allPermissionGranted() =
+        Constants.REQUIRED_PERMISSIONS.all{ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED}
+
+
 }
