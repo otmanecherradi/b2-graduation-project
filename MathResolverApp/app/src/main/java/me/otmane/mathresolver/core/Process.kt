4fd8cfcfc9a2d670b737dacc7111344f6a813d7a
Otmane <51253168+otmanecherradi@users.noreply.github.com>
2022-06-08 23:28:26 +0100

    spaces bug

--- MathResolverApp/app/src/main/java/me/otmane/mathresolver/core/Process.kt
+++ MathResolverApp/app/src/main/java/me/otmane/mathresolver/core/Process.kt
@@ -3,17 +3,21 @@ package me.otmane.mathresolver.core
 import me.otmane.mathresolver.models.Equation
 import me.otmane.mathresolver.models.EquationType
 
-val simpleEquationRegex = """(\d+.?\d+|\d+)([+\-x×÷/])(\d+.?\d+|\d+)""".toRegex()
+val simpleEquationRegex = """(\d+.?\d+|\d+)([+\-*x×÷/])(\d+.?\d+|\d+)""".toRegex()
 val firstDegreeEquationRegex = """(\d+.?\d+|\d+)x([+\-])(\d+.?\d+|\d+)=0""".toRegex()
 val secondDegreeEquationRegex =
     """(\d+.?\d+|\d+)x\^2([+\-])(\d+.?\d+|\d+)x([+\-])(\d+.?\d+|\d+)=0""".toRegex()
 
 class Process {
     companion object {
+        private fun cleanString(str: String): String {
+            return str.replace(" ", "").trim()
+        }
+
         fun getEquation(equationText: String, type: EquationType): Equation {
             val eq = Equation()
             eq.typeEnum = type
-            eq.text = equationText
+            eq.text = cleanString(equationText)
 
             return eq
         }
@@ -29,14 +33,36 @@ class Process {
         fun getEquationType(str: String): EquationType? {
             var type: EquationType? = null
 
-            if (simpleEquationRegex.matches(str)) {
+            val equationText = cleanString(str)
+
+            if (simpleEquationRegex.matches(equationText)) {
                 type = EquationType.Simple
-            } else if (firstDegreeEquationRegex.matches(str)) {
+            } else if (firstDegreeEquationRegex.matches(equationText)) {
                 type = EquationType.FirstDegree
-            } else if (secondDegreeEquationRegex.matches(str)) {
+            } else if (secondDegreeEquationRegex.matches(equationText)) {
                 type = EquationType.SecondDegree
             }
             return type
+        }
+
+        fun calculateResult(equation: Equation): String {
+            var result: Double = 0.0
+
+            if (equation.typeEnum == EquationType.Simple) {
+                val equationText = cleanString(equation.text)
+                val (nbr1, operation, nbr2) = simpleEquationRegex.find(equationText)!!.destructured
+
+                when (operation) {
+                    "+" -> result = nbr1.toDouble() + nbr2.toDouble()
+                    "-" -> result = nbr1.toDouble() - nbr2.toDouble()
+                    "×" -> result = nbr1.toDouble() * nbr2.toDouble()
+                    "x" -> result = nbr1.toDouble() * nbr2.toDouble()
+                    "*" -> result = nbr1.toDouble() * nbr2.toDouble()
+                    "/" -> result = nbr1.toDouble() / nbr2.toDouble()
+                    "÷" -> result = nbr1.toDouble() / nbr2.toDouble()
+                }
+            }
+            return "$result"
         }
     }
 }
